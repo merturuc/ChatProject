cea429fed0c42070cc3bd1353ea672f06ce19104
Furkan Guner <furkan-guner@hotmail.com>
2020-03-28 17:00:06 +0300

    Güncel Register

---
+++ app/src/main/java/com/example/deneme/RegisterActivity.java
@@ -0,0 +1,103 @@
+package com.example.deneme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterActivity extends Activity {
+    private EditText username, email, password, c_Password;
+    private Button registerButton;                              //tanımlamalar
+    private ProgressBar barLoading;
+    private static String urlRegister = "http://192.168.1.101/PHP/Register.php";
+
+    @Override
+    protected void onCreate(Bundle savedIntanceState) {
+        super.onCreate(savedIntanceState);
+        setContentView(R.layout.activity_register);  //id match here.
+        barLoading = findViewById(R.id.barLoading);
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.eMail);
+        password = findViewById(R.id.password);
+        c_Password = findViewById(R.id.c_Password);
+        registerButton = findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerMoment();  //While click your register button, what do u want ur app.
+            }
+        });
+    }
+
+    private void registerMoment() {
+        barLoading.setVisibility(View.VISIBLE);
+        registerButton.setVisibility(View.GONE);
+
+        final String username = this.username.getText().toString().trim();
+        final String email = this.email.getText().toString().trim();
+        final String password = this.password.getText().toString().trim();
+        final String c_password = this.c_Password.getText().toString().trim();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, urlRegister,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+
+                            if(success.equals("1")){
+                                Toast.makeText(RegisterActivity.this,"Register Success",Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(RegisterActivity.this,"Register Error" + e.toString(),Toast.LENGTH_SHORT).show();
+                            barLoading.setVisibility(View.GONE);
+                            registerButton.setVisibility(View.VISIBLE);
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(RegisterActivity.this,"Register Error" + error.toString(),Toast.LENGTH_SHORT).show();
+                        barLoading.setVisibility(View.GONE);
+                        registerButton.setVisibility(View.VISIBLE);
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("username",username);
+                params.put("email",email);
+                params.put("password",password);
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+}
